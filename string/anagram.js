a34cc5e7fb2a854ccc5eeaef78c0be5e11c62517
Anurag62k <anuragkumaraur19@gmail.com>
2026-01-07 20:44:12 +0530

    removed extra spaces

--- string/anagram.js
+++ string/anagram.js
@@ -1,8 +1,5 @@
 let str1 = prompt("Enter first string:");
 let str2 = prompt("Enter second string:");
-
-
-
 if (str1.length !== str2.length) {
   console.log("Not anagrams");
 } else {
@@ -10,18 +7,15 @@ if (str1.length !== str2.length) {
 
   for (let i = 0; i < str1.length; i++) {
     let count1 = 0, count2 = 0;
-
     for (let j = 0; j < str1.length; j++) {
       if (str1[i] === str1[j]) count1++;
       if (str1[i] === str2[j]) count2++;
     }
-
     if (count1 !== count2) {
       isAnagram = false;
       break;
     }
   }
-
   if (isAnagram) {
     console.log("Strings are anagrams");
   } else {
